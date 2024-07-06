b3e6595c5c5d4b497ba21fb4de842498785f5e26
anantsrivastava02 <itanart333@gmail.com>
2024-07-06 18:51:00 +0530

    strucure update

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Anant Srivastava',
+    author_email="itanart333@gmail.com",
+    install_requires=[
+        'openai',
+        'langchain',
+        'streamlit',
+        'python-dotenv',
+        'PyPDF2'
+    ],
+    packages=find_packages(),
+)
+
