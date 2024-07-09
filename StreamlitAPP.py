21871ccec5ee795d38528a2814eefa885588af83
anantsrivastava02 <itanart333@gmail.com>
2024-07-09 19:02:23 +0530

    corrected  the codes

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 from src.mcqgenrator.utils import read_file, get_table_data
 import streamlit as st 
 from langchain.callbacks import get_openai_callback
-from mcqgenrator.MCQGenerator import evalute_chains
-from mcqgenrator.logger import logging
+from src.mcqgenrator.MCQGenerator import evalute_chains
+from src.mcqgenrator.logger import logging
 
 with open('C:/Users/anant/mcq_gen/Response.json', 'r') as file:
     RESPONSE_JSON = json.load(file)
